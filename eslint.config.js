a92193ce66e9ada2af376070a942744a850f9f81
neno <github@meaning.systems>
2025-05-22 14:43:38 +0200

    chore: add TypeScript lint setup

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    rules: {}
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', 'claude-task-master/**']
+  }
+];
